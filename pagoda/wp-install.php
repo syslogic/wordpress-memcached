de6e947a55e1ec5ad94a6ba182b6248986064c5d
syslogic <martin@codefx.biz>
2012-10-17 22:34:52 +0200

    boxfile & installer updated; yet untested.

--- pagoda/wp-install.php
+++ pagoda/wp-install.php
@@ -1,45 +1,43 @@
 <?php
 /*
-	WordPress Installer for Pagoda Box v1.05
+	WordPress with Memcached Object Cache
+	Installer for Pagoda Box v1.05
 	Copyright 2012 by Martin Zeitler
 	http://codefx.biz/contact
 */
 
 /* the environment */
-$fn='latest.zip';
+$fn1='latest.zip';
+$src1='http://wordpress.org/'.$fn1;
+$fn2='memcached.2.0.2.zip';
+$src2='http://downloads.wordpress.org/plugin/'.$fn2;
 $base_dir = str_replace('/pagoda','', dirname(__FILE__));
-$src='http://wordpress.org/'.$fn;
-$dst=$base_dir.'/pagoda/wp-'.$fn;
+$v_info=dirname(__FILE__).'/wordpress/wp-includes/version.php';
+$dst1=$base_dir.'/pagoda/'.$fn1;
+$dst2=$base_dir.'/pagoda/'.$fn2;
 
-/* fetch the package */
-if(file_exists($dst)){unlink($dst);}
-$fp = fopen($dst, 'w');
-$curl = curl_init();
-$opt = array(CURLOPT_URL => $src, CURLOPT_HEADER => false, CURLOPT_FILE => $fp);
-curl_setopt_array($curl, $opt);
-$rsp = curl_exec($curl);
-if($rsp===false){
-	die("[cURL] errno:".curl_errno($curl)."\n[cURL] error:".curl_error($curl)."\n");
-}
-$info = curl_getinfo($curl);
-curl_close($curl);
-fclose($fp);
+/* fetch the packages */
+retrieve($src1, $dst1);
+retrieve($src1, $dst1);
 
-/* cURL stats */
-$time = $info['total_time']-$info['namelookup_time']-$info['connect_time']-$info['pretransfer_time']-$info['starttransfer_time']-$info['redirect_time'];
-echo "Fetched '$src' @ ".abs(round(($info['size_download']*8/$time/1024/1024/1024),2))."GBps.\n";
-
-/* unzip the package */
+/* unzip the WordPress package */
 $zip = new ZipArchive;
-if($zip->open($dst) === TRUE) {
+if($zip->open($dst1) === TRUE) {
 	$zip->extractTo(dirname(__FILE__));
 	$zip->close();
 }
 
+/* unzip the Memcached Object Cache plugin */
+$zip = new ZipArchive;
+if($zip->open($dst2) === TRUE) {
+	$zip->extractTo(dirname(__FILE__).'/wordpress/wp-content/plugins');
+	$zip->close();
+}
+
 /* retrieve version number */
-if(file_exists(dirname(__FILE__).'/wordpress/wp-includes/version.php')){
-	require_once(dirname(__FILE__).'/wordpress/wp-includes/version.php');
-	echo 'WordPress v'.$wp_version.' will now be deployed & configured.';
+if(file_exists($v_info)){
+	require_once($v_info);
+	echo 'WordPress v'.$wp_version.' with Memcached Object Cache v2.0.2 will now be deployed.';
 }
 
 function format_size($size=0) {
@@ -52,5 +50,23 @@ function format_size($size=0) {
 	else {
 		return round($size/1048576,2).'mb';
 	}
+}
+
+function retrieve($src, $dst){
+	$fp = fopen($dst, 'w');
+	$curl = curl_init();
+	$opt = array(CURLOPT_URL => $src, CURLOPT_HEADER => false, CURLOPT_FILE => $fp);
+	curl_setopt_array($curl, $opt);
+	$rsp = curl_exec($curl);
+	if($rsp===false){
+		die("[cURL] errno:".curl_errno($curl)."\n[cURL] error:".curl_error($curl)."\n");
+	}
+	$info = curl_getinfo($curl);
+	curl_close($curl);
+	fclose($fp);
+	
+	/* cURL stats */
+	$time = $info['total_time']-$info['namelookup_time']-$info['connect_time']-$info['pretransfer_time']-$info['starttransfer_time']-$info['redirect_time'];
+	echo "Fetched '$src' @ ".abs(round(($info['size_download']*8/$time/1024/1024/1024),2))."GBps.\n";
 }
 ?>
