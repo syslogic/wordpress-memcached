d7fa52dc872a880b118d01e18dd71a1279f65a0f
syslogic <martin@codefx.biz>
2012-10-17 23:33:29 +0200

    installer updated once more.

--- pagoda/wp-install.php
+++ pagoda/wp-install.php
@@ -13,6 +13,7 @@ $src1='http://wordpress.org/'.$fn1;
 $src2='http://downloads.wordpress.org/plugin/'.$fn2;
 $base_dir = str_replace('/pagoda','', dirname(__FILE__));
 $version_info=dirname(__FILE__).'/wordpress/wp-includes/version.php';
+$plugins=dirname(__FILE__).'/wordpress/wp-content/plugins';
 $dst1=$base_dir.'/pagoda/'.$fn1;
 $dst2=$base_dir.'/pagoda/'.$fn2;
 
@@ -20,23 +21,23 @@ $dst2=$base_dir.'/pagoda/'.$fn2;
 retrieve($src1, $dst1);
 retrieve($src2, $dst2);
 
-/* unzip the WordPress package */
+/* extract the main package */
 $zip = new ZipArchive;
 if($zip->open($dst1) === TRUE) {
 	$zip->extractTo(dirname(__FILE__));
 	$zip->close();
 }
 
-/* patch the plugin */
+/* extract the plug-in package */
 $zip = new ZipArchive;
 if($zip->open($dst2) === TRUE) {
-	$zip->extractTo(dirname(__FILE__).'/wordpress/wp-content/plugins');
+	$zip->extractTo($plugins);
 	$zip->close();
-	mkdir(dirname(__FILE__).'/wp-content/plugins/flexicache/_data');
+	mkdir($plugins.'/flexicache/_data');
 }
 
 /* remove useless files */
-unlink(dirname(__FILE__).'/wordpress/wp-content/plugins/hello.php');
+unlink($plugins.'/hello.php');
 
 /* retrieve version number */
 if(file_exists($version_info)){
